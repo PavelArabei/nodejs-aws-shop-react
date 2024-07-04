da9513a3eea66a205a7779d77663f8eb8034c02c
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-04 18:31:41 +0200

    fix: fix apiPaths.ts

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://qdnnjt6el2.execute-api.eu-west-1.amazonaws.com/prod/",
+  product: "https://zvpee75h36.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://odsabsyg65.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
